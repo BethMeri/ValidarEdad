ec221973930741b64dc3cd6010032e352d427480
Beth <betzabeth290104@gmail.com>
2025-05-22 17:12:43 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'validarEdad',
+  webDir: 'www'
+};
+
+export default config;
